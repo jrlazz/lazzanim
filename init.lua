ce488677ba4af02a1da44b77fe38ab4f99311ed9
jrlazz <50157295+jrlazz@users.noreply.github.com>
2019-06-01 15:06:13 -0300

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,39 @@
+local MOD_NAME=minetest.get_current_modname()
+local MOD_PATH=minetest.get_modpath(MOD_NAME)
+local WORLD_PATH=minetest.get_worldpath()
+lazzanim = {};
+local FORM_NAME="lazzanimform"
+local meu=0
+local conta=0
+local img="__.png"
+local para=0
+
+local function showForm(player)
+	local playerName=player:get_player_name()
+	if not playerName or playerName=="" then return;end
+	local fs="size[8,6]"
+	fs=fs.."label[1,1;Images from www.flaticon.com/icon-packs/emoji]"
+	fs=fs.."label[1,3;"..img.." ]"
+	fs=fs.."image[3,2.3;2,2;"..img.."]"
+
+	fs=fs.."label[2,5.5;... esc and right-click to restart]"
+function visual()
+	conta=conta+1
+	img="emoji_0"..conta..".png"
+	if conta==9 then conta=1;end
+	para=para+1
+	showForm(player)
+end
+
+	if para<55 then minetest.after(.1,visual);end
+
+	minetest.show_formspec(playerName, FORM_NAME, fs)
+end
+	
+lazzanim_emoji={}
+minetest.register_node("lazzanim:emoji",{
+	description="emoji",tile_images={"emoji_01.png"},
+	on_rightclick=function(pos,node,player,itemstack,pointedThing)
+	para=0 local handle=0 showForm(player)
+	end,}
+)
---
+++ readme.md
@@ -0,0 +1,22 @@
+﻿June 2019 - José Roberto Lazzareschi
+
+﻿
+:relaxed: Tested in Minetest 5.0.1
+
+lazznav is a funny form ("fufo").
+
+![alt text](https://raw.githubusercontent.com/jrlazz/lazznav/master/lazznav_img_0.jpg)
+
+![alt text](https://raw.githubusercontent.com/jrlazz/lazznav/master/naveA_naveB.jpg)
+
+Right clicking naveA or naveB blocks ... opens the Naval Battle fufo.
+
+![alt text](https://raw.githubusercontent.com/jrlazz/lazznav/master/lazznav_img_1.png)
+
+![alt text](https://raw.githubusercontent.com/jrlazz/lazznav/master/lazznav_img.png)
+
+Sam and his world do not make part of the fun, but without them, there were no funny forms.
+
+So I guess lazznav can´t be called a mod.
+
+The fufo is a pastime, maybe like solitaire, freecell, etc., and may or not be a game.
